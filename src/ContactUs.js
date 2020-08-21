b1b43f39a6150780e0ae6fc0742d54ec324cb008
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-08-20 22:49:54 -0400

    a

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -75,7 +75,6 @@ export const ListGrid = styled.div`
     gap: 30px;
     @media (max-width: 700px){
         padding-left: 50px;
-        width: 80%;
     }
 `
 
@@ -104,7 +103,6 @@ box-shadow: 0px 0px 4px 0px grey;
 @media (max-width: 700px){
     font-size: 15px;
     padding: 10px;
-    width: 80%;
 }
  `
  const DialogBannerName = styled(TextLabel)`
